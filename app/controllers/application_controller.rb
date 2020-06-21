8dd3322e6eef957a415528e591a71c0c9d94bbf7
taka.s <takapie81@gmail.com>
2020-06-21 05:31:58 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ private
     
   def counts(user)
       @count_microposts = user.microposts.count
+      @count_followings = user.followings.count
+      @count_followers = user.followers.count
   end
 
   def require_user_logged_in
